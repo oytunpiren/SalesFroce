4fc57b30c14e4ec46f3d3cf02dd499b66851b4d4
oytun <oytunpiren@gmail.com>
2020-04-15 02:33:59 -0500

    Login Page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,22 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends  BasePage {
+
+
+    @FindBy (id="ct100_MainContent_username")
+    public WebElement userNmaeInputBoxElement;
+
+
+    @FindBy(id="ct100_MainContent_password")
+    public WebElement passwordInputBoxElement;
+
+
+
+    public void login(String username, String password){
+        userNmaeInputBoxElement.sendKeys(username);
+        passwordInputBoxElement.sendKeys(password);
+    }
 }
--- src/main/java/step_definations/LoginStepDefinations.java
+++ src/main/java/step_definations/LoginStepDefinations.java
@@ -2,12 +2,16 @@ package step_definations;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import utilities.ConfigurationReader;
+import utilities.Driver;
 
 public class LoginStepDefinations {
 
     @Given("user is on the login page")
-    public void user_is_on_the_login_page() {
-
+    public void user_is_on_the_login_page() throws InterruptedException {
+        String url = ConfigurationReader.getProperty("url");
+        Driver.getDriver().get(url);
+        Thread.sleep(1000);
     }
 
 
@@ -15,13 +19,6 @@ public class LoginStepDefinations {
     public void user_logs_in() {
 
     }
-
-
-
-
-
-
-
 
 
 
