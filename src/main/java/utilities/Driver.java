b1182e1aeabf5abd6da43315c6eed7a190f4c071
oytun <oytunpiren@gmail.com>
2020-04-15 01:35:04 -0500

    First Commit

---
+++ src/main/java/utilities/Driver.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    private Driver() {
+
+    }
+
+    public WebDriver getDriver() {
+        if (driver == null) {
+            String browser = ConfigurationReader.getProperty("browser");
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new RuntimeException("Wring Browser Name!!!");
+            }
+        }
+
+return driver;
+    }
+
+
+}
