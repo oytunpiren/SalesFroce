c00e25281c2057d9389358b2b26a46ade9204b00
oytun <oytunpiren@gmail.com>
2020-04-15 02:19:14 -0500

    runner class

---
+++ src/main/java/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+glue = "step_defonations",
+features = "src/main/resources/features"
+)
+
+
+public class CukesRunner {
+
+
+
+
+
+}
